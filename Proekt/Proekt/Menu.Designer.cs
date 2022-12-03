9308f8ee68f5cded3d9a0b7804ab9258f097764a
Олег Грушко <grushkoov@yandex.ru>
2022-12-03 09:28:32 +0500

    переделать

--- Proekt/Proekt/Menu.Designer.cs
+++ Proekt/Proekt/Menu.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Proekt
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -40,6 +40,8 @@ namespace Proekt
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -97,40 +99,59 @@ namespace Proekt
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(278, 212);
+            this.textBox6.Location = new System.Drawing.Point(12, 213);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(114, 20);
             this.textBox6.TabIndex = 7;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(12, 212);
+            this.textBox7.Location = new System.Drawing.Point(132, 212);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(114, 20);
             this.textBox7.TabIndex = 8;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(132, 212);
+            this.textBox8.Location = new System.Drawing.Point(252, 213);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(114, 20);
             this.textBox8.TabIndex = 9;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(372, 80);
+            this.button2.Location = new System.Drawing.Point(372, 121);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(112, 20);
             this.button2.TabIndex = 10;
-            this.button2.Text = "button2";
+            this.button2.Text = "Стереть";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(372, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 20);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(529, 122);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(114, 20);
+            this.textBox9.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
@@ -144,6 +165,7 @@ namespace Proekt
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,6 +184,8 @@ namespace Proekt
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox9;
     }
 }
 
---
+++ Proekt/Proekt/proekt2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Proekt
+{
+    public partial class proekt2 : Form
+    {
+        MySql sql;
+        MySqlConnection conn;
+        class MySql
+        {
+            string h = "chuc.caseum.ru";
+            string p = "33333";
+            string u = "uchebka";
+            string bd = "uchebka";
+            string P = "uchebka";
+            public string connStr;
+            public string con()
+            {
+                return connStr = $"server={h};port" +
+                     $"={p};user={u};database={bd};password={P};";
+            }
+        }
+        
+        public proekt2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sql = new MySql();
+                sql.con();
+                conn = new MySqlConnection(sql.connStr);
+                conn.Open();
+                conn.Close();
+                MessageBox.Show("Все чики пуки");
+            }
+            catch
+            {
+                MessageBox.Show("Все плохо");
+            }
+        }
+    }
+}
