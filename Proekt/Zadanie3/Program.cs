49a32f15afa9442c5bcb1bb3bb7e67e196037961
Олег Грушко <grushkoov@yandex.ru>
2022-12-05 16:43:55 +0300

    Сделал 3 задание( еле как ,я плакал)

--- Proekt/Zadanie3/Program.cs
+++ Proekt/Zadanie3/Program.cs
@@ -12,11 +12,28 @@ namespace Zadanie3
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form());
+        }
+
+
+    }
+    public class Connect
+    {
+        
+        public string h = "chuc.caseum.ru";
+        public string p = "33333";
+        public string u = "st_1_20_9";
+        public string d = "is_1_20_st9_KURS";
+        public string P = "19134029";
+        public string connStr;
+        public string con()
+        {
+            return connStr = $"server={h};port={p};user={u};database={d};password={P};";
         }
     }
 }
